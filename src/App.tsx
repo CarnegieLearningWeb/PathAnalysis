a2143dc48d11e61c2f4673333cc0e2f83f7dd4de
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-05-31 10:19:53 -0400

    migrated files over to public

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import './App.css'
+import { 
+  // useDataShopData, 
+  useLocalSampleData,
+  usePathAnalysisData 
+} from '@/lib/dataFetchingHooks';
+import { useEffect, useState } from 'react';
+import { convertDataTypesIncomingData, processPathAnalysisData, processDataShopData } from '@/lib/dataProcessingUtils';
+import { GraphData } from './lib/types';
+import DirectedGraph from './components/DirectedGraph';
+
+function App() {
+  const sectionId = "area_perimeter_mix_rectangle_derived";
+  const problemId = "area_perimeter_mix_rectangle_derived-020";
+
+  const { pathAnalysisData, isPathAnalysisDataLoading } = usePathAnalysisData(sectionId, problemId);
+  // const { dataShopData, isDataShopDataLoading } = useDataShopData();
+  const filePath = "analyzing_different_forms_of_expressions.json"
+  const { localSampleData, isLocalSampleDataLoading } = useLocalSampleData(filePath)
+  const [processedPathAnalysisData, setProcessedPathAnalysisData] = useState<GraphData | null>(null);
+  const [processedShopData, setProcessedShopData] = useState<GraphData | null>(null);
+
+  // useEffect( () => {
+  //   if (isDataShopDataLoading) {
+  //     return;
+  //   }
+  //   else{
+  //     console.log("dataShopData: ", dataShopData);
+  //   }
+  // }, [isDataShopDataLoading, dataShopData])
+
+  useEffect(() => {
+    if (isLocalSampleDataLoading) {
+      return;
+    }
+    if (localSampleData) {
+      const _processData = async () => {
+        const processedData = await processDataShopData(localSampleData)
+        setProcessedShopData(processedData);
+      }
+      _processData();
+    }
+  }, [isLocalSampleDataLoading, localSampleData])
+
+
+  useEffect(() => {
+    const _processData = async () => {
+      if (pathAnalysisData) {
+        setProcessedPathAnalysisData(await processPathAnalysisData(convertDataTypesIncomingData(pathAnalysisData)));
+      }
+    }
+    _processData();
+
+  }, [isPathAnalysisDataLoading, pathAnalysisData])
+
+  if (isPathAnalysisDataLoading || !processedPathAnalysisData || !processedShopData) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <>
+      <div className="p-5">
+        {/* path analysis window */}
+
+        <DirectedGraph graphData={processedShopData} />
+      </div>
+
+    </>
+  )
+}
+
+export default App
