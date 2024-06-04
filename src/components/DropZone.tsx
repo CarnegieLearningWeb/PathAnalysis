91852602dc271da369d638e2f300eae8f88043e6
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-06-04 15:09:00 -0400

    added file reader

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -2,9 +2,21 @@ import { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 
 export default function DropZone() {
-    const onDrop = useCallback((acceptedFiles: any) => {
-        console.log(acceptedFiles);
-    }, []);
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        acceptedFiles.forEach((file: File) => {
+            const reader = new FileReader()
+
+            reader.onabort = () => console.log('file reading was aborted')
+            reader.onerror = () => console.log('file reading has failed')
+            reader.onload = () => {
+                // Do whatever you want with the file contents
+                const binaryStr = reader.result
+                console.log(binaryStr)
+            }
+            reader.readAsArrayBuffer(file)
+        })
+
+    }, [])
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
