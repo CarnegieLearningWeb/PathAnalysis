6d44efa5c802e983c48a8c138544ed77620ade78
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-06-06 15:32:00 -0400

    Context template for app

---
+++ src/Context.tsx
@@ -0,0 +1,21 @@
+import { createContext } from 'react';
+interface ContextInterface {
+
+}
+export const Context = createContext({} as ContextInterface);
+
+const initialState = {
+
+}
+
+interface ProviderProps {
+    children: React.ReactNode;
+}
+export const Provider = ({ children }: ProviderProps) => {
+    return (
+        <Context.Provider
+            value={ }
+        >
+            {children}
+        </Context.Provider>
+}
