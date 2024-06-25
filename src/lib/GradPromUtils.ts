05cdbe908c12e21cd9993b287cf0f3f1475c6b6d
talizacks <55198700+talizacks@users.noreply.github.com>
2024-06-25 14:15:20 -0500

    Filter Promoted and Graduated

--- src/lib/GradPromUtils.ts
+++ src/lib/GradPromUtils.ts
@@ -1,7 +1,11 @@
 import { DataSet1 } from "./types";
 
 
-function promotedFilter(data: DataSet1): DataSet1 {
-    
-    return
+export function filterPromGrad(data: DataSet1[], promOrGrad: "GRADUATED" | "PROMOTED" | "NOT_COMPLETED" | null): DataSet1[] {
+    if (promOrGrad !== null) {
+        return data.filter(item => item["CF (Workspace Progress Status)"] === promOrGrad);
+    } else {
+        // Return the original data if promOrGrad is null
+        return data;
+    }
 }
