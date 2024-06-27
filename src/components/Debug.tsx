2acb5956f700c0824650957491ecf5ac3178d5e8
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-06-27 10:53:10 -0400

    fixed error where .tsv files were not accepted

--- src/components/Debug.tsx
+++ src/components/Debug.tsx
@@ -1,7 +1,22 @@
-export default function Debug(){
-    return(
+import { GlobalDataType } from "@/lib/types";
+import { useState } from "react";
+import DropZone from "./DropZone";
+
+export default function Debug() {
+    const [data, setData] = useState<GlobalDataType[]>([])
+    const handleData = (data: GlobalDataType[]) => {
+        setData(data)
+        console.log("Data from file: ", data);
+        
+    }
+
+    const handleLoading = (loading: boolean) => {
+    }
+
+    return (
         <>
-        Debug Page
+
+            <DropZone afterDrop={handleData} onLoadingChange={handleLoading} />
         </>
     )
 }
