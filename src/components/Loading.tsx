6e15eb5c802babb829b5ce1eb1fc3a1e3c398c59
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-06-27 10:53:10 -0400

    fixed error where .tsv files were not accepted

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+        <div className="absolute top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center">
+            <div className="bg-white p-4 rounded-lg">
+                <p>Loading...</p>
+            </div>
+        </div>
+    )
+}
