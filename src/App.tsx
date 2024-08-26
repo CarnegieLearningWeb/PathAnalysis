c67061b715b0fe0f2de05ab6ea158abf3ca706ca
talizacks <55198700+talizacks@users.noreply.github.com>
2024-08-26 12:57:51 -0500

    Still trying to get color to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useCallback, useState } from 'react';
 import Upload from "@/components/Upload.tsx";
-import GraphvizParent from "@/components/GraphvizParent.tsx";
+import GraphvizParent, {SequenceCount} from "@/components/GraphvizParent.tsx";
 import FilterComponent from './components/FilterComponent.tsx';
 import SelfLoopSwitch from './components/selfLoopSwitch.tsx';
 import Slider from './components/slider.tsx';
@@ -12,12 +12,12 @@ const App: React.FC = () => {
     const [filter, setFilter] = useState<string>('');
     const [selfLoops, setSelfLoops] = useState<boolean>(true);
     const [minVisits, setMinVisits] = useState<number>(10);
-    const [topSequences, setTopSequences] = useState<string[][]>([]);
-    const [selectedSequence, setSelectedSequence] = useState<string[]>(topSequences[0]);
+    const [topSequences, setTopSequences] = useState<SequenceCount>([]);
+    const [selectedSequence, setSelectedSequence] = useState<string[]>(['']);
 
     const handleSelectSequence = (selectedSequence: string[]) => {
         setSelectedSequence(selectedSequence); // Fix: Use the correct parameter to update the state
-        console.log(`Selected sequence: ${sequence}`);
+        console.log(`Selected sequence: ${selectedSequence}`);
     };
 
     const handleToggle = () => setSelfLoops(!selfLoops);
@@ -33,9 +33,9 @@ const App: React.FC = () => {
         <div>
             <h1>Path Analysis Tool</h1>
             <Upload onDataProcessed={handleDataProcessed} />
+            <Slider step={5} min={0} max={500} value={minVisits} onChange={handleSlider}/>
             <FilterComponent onFilterChange={setFilter} />
             <SequenceSelector onSelectSequence={handleSelectSequence} sequences={topSequences} />
-            <Slider step={5} min={0} max={500} value={minVisits} onChange={handleSlider} />
             <SelfLoopSwitch isOn={selfLoops} handleToggle={handleToggle} />
             <GraphvizParent
                 csvData={csvData}
