032f3d97891e5757bb4b12695188e5c1b2265d3a
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-12-03 16:46:12 -0500

    reformat some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css';
-import { useContext, useEffect, useState } from 'react';
-import { GlobalDataType, GraphData } from './lib/types';
+import {useContext, useEffect, useState} from 'react';
+import {GlobalDataType, GraphData} from './lib/types';
 import DropZone from './components/DropZone';
 
-import { Button } from './components/ui/button';
-import { Context } from './Context';
-import { processDataShopData } from './lib/dataProcessingUtils';
+import {Button} from './components/ui/button';
+import {Context} from './Context';
+import {processDataShopData} from './lib/dataProcessingUtils';
 import Loading from './components/Loading';
 
 function App() {
 
-    const { resetData, setGraphData, setLoading, data, setData, loading, error, setError } = useContext(Context);
+    const {resetData, setGraphData, setLoading, data, setData, loading, error, setError} = useContext(Context);
     const [showDropZone, setShowDropZone] = useState<boolean>(true);
     const handleData = (data: GlobalDataType[]) => {
         setData(data)
@@ -48,21 +48,22 @@ function App() {
                     Reset
                 </Button>
                 {error && (
-                        <div className="text-red-500 p-4 m-4 bg-red-50 rounded-md">
-                            {error.split('\n').map((errorLine, index) => (
-                                <p key={index} className="mb-1">{errorLine}</p>
-                            ))}
-                        </div>
-                    )}
+                    <div className="text-red-500 p-4 m-4 bg-red-50 rounded-md">
+                        {error.split('\n').map((errorLine, index) => (
+                            <p key={index} className="mb-1">{errorLine}</p>
+                        ))}
+                    </div>
+                )}
                 <div className=" flex items-center justify-center pt-20">
                     {
                         loading ?
-                            <Loading />
+                            <Loading/>
                             :
                             (
                                 showDropZone && (
                                     <div className="">
-                                        <DropZone afterDrop={handleData} onLoadingChange={handleLoading} onError={handleError} />
+                                        <DropZone afterDrop={handleData} onLoadingChange={handleLoading}
+                                                  onError={handleError}/>
                                     </div>
                                 )
 
@@ -70,7 +71,6 @@ function App() {
 
                     }
 
-                  
 
                     {
                         data && <div>
